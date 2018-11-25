4d2232109eec12e0e9dc07d9e91ac8e68c2d2ccd
202113313@qq.com <FengJue123>
2018-11-25 16:11:39 +0800

    创建单利 装饰器

--- 1.py
+++ 1.py
@@ -1,4 +1,43 @@
-a = '123'
+def extra(func):
+    def sing():
+        print('sing')
+        func()
 
-for x in a:
-    print(x)
+    return sing
+
+
+@extra
+def speak():
+    print("speak")
+
+
+speak()
+
+
+def ext():
+    def sing():
+        print('1')
+
+    return sing
+
+
+ex = ext()
+ex()
+
+
+class Danli(object):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance == None:
+            cls._instance = super().__new__(cls)
+            return cls._instance
+
+        return cls._instance
+
+
+d1 = Danli()
+d2 = Danli()
+
+print(d1)
+print(d2)
--- 2.py
+++ 2.py
@@ -1,1 +1,24 @@
-print(22233)
+class People(object):
+
+    def __init__(self):
+        self.v = 100
+
+    # def __new__(cls, *args, **kwargs):
+    #     print('123')
+
+        # return cls
+
+    @property
+    def price(self):
+        return self.v
+
+    @price.setter
+    def price(self, value):
+        self.v = value
+
+
+p = People()
+
+print(p.price)
+p.price = 200
+print(p.price)
